33591a45697251542277c2966b07e29b8ee39267
wRadion <wradion@gmail.com>
2020-12-05 16:34:25 +0100

    Remove console log

--- src/roles_updater.js
+++ src/roles_updater.js
@@ -41,7 +41,6 @@ function getUserInfos(user, url, langId, logFunction) {
 
     // Check if user has done at least one test
     const testsTaken = parseInt((await page.$('#profile-data-table > tbody > tr:nth-child(7) > td:nth-child(2)', n => n.innerText)).replace(/,/,''));
-    console.log(testsTaken);
     if (testsTaken <= 0) {
       // Reject Promise
       reject("You need to do at least one test on 10FF to have a WPM role (competitions are excluded).");
